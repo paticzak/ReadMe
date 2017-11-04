b18ec3afd4af40cfe5114b92b4234d29f42a4ee9
paticzak <p.mundzik@gmail.com>
2017-11-04 20:43:27 +0100

    Basic version with first exercise almost done

--- ReadMe/Controllers/BooksController.cs
+++ ReadMe/Controllers/BooksController.cs
@@ -48,21 +48,6 @@ namespace ReadMe.Controllers
         }
           
 
-
-
-        //// books
-        //public ActionResult Index(int? pageIndex, string sortBy)
-        //{
-        //    if (!pageIndex.HasValue)
-        //        pageIndex = 1;
-
-        //    if (String.IsNullOrWhiteSpace(sortBy))
-        //        sortBy = "Name";
-
-        //    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-
-        //}
-
         //[Route("books/released/{year}/{month:regex(\\d{2}:range(1,12)}")]
         //public ActionResult ByReleaseDate(int year, int month)
         //{
--- ReadMe/Models/User.cs
+++ ReadMe/Models/User.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿rewqusing System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
