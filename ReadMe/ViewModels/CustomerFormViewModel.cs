b3934d186f579758b77621afe2f742426bf1f054
paticzak <p.mundzik@gmail.com>
2017-11-13 09:31:39 +0100

    Adding all files in project

---
+++ ReadMe/ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ReadMe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReadMe.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+
+    }
+}
