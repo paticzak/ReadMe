4edc7e3341f240af501234eb59d824d63e02f4fd
paticzak <p.mundzik@gmail.com>
2017-11-18 18:23:23 +0100

    Aktualizacja - dodanie wszystkich plików do projektu

---
+++ ReadMe/ViewModels/NewBookViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ReadMe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReadMe.ViewModels
+{
+    public class NewBookViewModel
+    {
+        public IEnumerable<Genre> Genres { get; set; }
+        public Book Book { get; set; }
+
+
+    }
+}
