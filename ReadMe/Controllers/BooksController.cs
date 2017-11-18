f86ca2d7a94d16f0b160766a8cd151f0f42268ac
paticzak <p.mundzik@gmail.com>
2017-11-18 17:00:46 +0100

    Adding Book to database

--- ReadMe/Controllers/BooksController.cs
+++ ReadMe/Controllers/BooksController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using ReadMe.Models;
 using ReadMe.ViewModels;
+using System.Data.Entity.Validation;
 
 namespace ReadMe.Controllers
 {
@@ -23,10 +24,53 @@ namespace ReadMe.Controllers
             _context.Dispose();
         }
 
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+            var viewModel = new NewBookViewModel
+            {
+                Genres = genres
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Book book)
+        {
+            if (book.Id == 0)
+                _context.Books.Add(book);
+            else
+            {
+                var bookInDb = _context.Books.Single(b => b.Id == book.Id);
+                bookInDb.Name = book.Name;
+                bookInDb.Author = book.Author;
+                bookInDb.Genre = book.Genre;
+            }
+
+            _context.SaveChanges();       
+
+            return RedirectToAction("Index", "Books");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var book = _context.Books.SingleOrDefault(b => b.Id == id); // get the books with id from the database
+
+            if (book == null)
+                return HttpNotFound();
+
+            var viewModel = new NewBookViewModel
+            {
+                Book = book,
+                Genres = _context.Genres.ToList()
+            };
+            return View("New", viewModel);
+        }
+
         // GET: /Users/
         public ActionResult Index()
         {
-            var books = _context.Books.ToList();
+            var books = _context.Books.Include(b => b.Genre).ToList();
 
             return View(books);
         }
--- ReadMe/Models/Book.cs
+++ ReadMe/Models/Book.cs
@@ -9,11 +9,16 @@ namespace ReadMe.Models
     public class Book
     {
         public int Id { get; set; }
+
+        [Display(Name = "Title")]
         public string Name { get; set; }
+
         public string Author { get; set; }
+       
+        public Genre Genre { get; set; }
 
         [Required]
-        public Genre Genre { get; set; }
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
 
 
