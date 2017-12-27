24d35e042cef64ba7a99fec2957945b2be3b67bd
paticzak <p.mundzik@gmail.com>
2017-12-27 21:18:03 +0100

    Working with GeNie network: get and view simple data in app

---
+++ ReadMe/Controllers/BayesianNetworkController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Smile;
+using Smile.Learning;
+
+namespace ReadMe.Controllers
+{
+    public class BayesianNetworkController : Controller
+    {
+        [AllowAnonymous]
+        // GET: /BayesianNetwork/
+        public ActionResult Index()
+        {
+            try
+            {
+                //Network net = new Network();
+                //net.ReadFile(@"C:\Users\Patrycja\Desktop\Praca inzynierska\nastroj-ksiazka-kategoria-okrojone-poprawione.xdsl");
+
+            }
+            catch (SmileException e)
+            {
+                return View(e.Message);
+            }
+           
+            return View();
+        }
+	}
+}
