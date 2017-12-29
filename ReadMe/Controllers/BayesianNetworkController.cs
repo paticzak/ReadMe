485bc8f5f296721b3ec48e4d32c8bef87200b327
paticzak <p.mundzik@gmail.com>
2017-12-29 10:33:42 +0100

    Working form genie usermood and booktype

--- ReadMe/Controllers/BayesianNetworkController.cs
+++ ReadMe/Controllers/BayesianNetworkController.cs
@@ -30,36 +30,80 @@ namespace ReadMe.Controllers
         }
 
         [HttpPost]
-        public ActionResult RenderForm(string jakiNastroj)
-        {
-            
+        public ActionResult RenderForm(string jakiNastroj, string bookType)
+        {            
             Network net = new Network();
-            ViewBag.Net = net;
 
             net.ReadFile(@"C:\Users\Patrycja\Desktop\Praca inzynierska\nastroj-ksiazka-kategoria-okrojone-poprawione.xdsl");
-            //net.ReadFile(@"C:\Users\Patrycja\Desktop\Praca inzynierska\ReadMe2.xdsl");
             net.UpdateBeliefs();
-            int outcomeIndex;
-            string nastroj1 = jakiNastroj;
-            //string nastroj1 = Request["jakiNastroj"];
 
-            // Getting the index of the "Failure" outcome:
-            net.SetEvidence("nastroj", nastroj1);
-            net.SetEvidence("wciagajaca", "tak");
+            //int outcomeIndex;
+            double[] FindBiggestValue = new double[10];
+            int i = 0;
+            
+            net.SetEvidence("nastroj", jakiNastroj);
+            net.SetEvidence(bookType, "tak");
             net.UpdateBeliefs();
-            String[] aGatunekOutcomeIds = net.GetOutcomeIds("refWciag");
-            for (outcomeIndex = 0; outcomeIndex < aGatunekOutcomeIds.Length; outcomeIndex++)
+
+            for (int NodeId = 7; NodeId <= 16; NodeId++)
             {
-                if ("tak".Equals(aGatunekOutcomeIds[outcomeIndex]))
-                {
-                    break;
-                }
+                String[] aThirdLayerOutcomeIds = net.GetOutcomeIds(net[NodeId]);
+                int outcomeIndex1;
+                for (outcomeIndex1 = 0; outcomeIndex1 < aThirdLayerOutcomeIds.Length; outcomeIndex1++)
+                    if ("tak".Equals(aThirdLayerOutcomeIds[outcomeIndex1]))
+                        break;
+
+                double[] pValues = net.GetNodeValue(net[NodeId]);
+                double P_ChosenBook = pValues[outcomeIndex1];
+
+                // DODAJĘ WARTOŚCI PRAWDOPODOBIEŃSTW DO TABLICY
+                FindBiggestValue[i] = P_ChosenBook;
+                i++;
+
+                //Console.WriteLine("P(Nastroj = {0} | JakaKsiazka = {1}) = | \"Gatunek\" {2}) = {3}", userinputNastroj, userInputJakaKsiazka, net[NodeId], P_ChosenBook);
             }
 
-            double[] aValues = net.GetNodeValue("refWciag");
-            double P_ChosenBook = aValues[outcomeIndex];
+            // ZNAJDUJĘ WARTOŚĆ ORAZ POZYCJĘ NAJWIĘKSZEGO PRAWDOPODOBIEŃSTWA W TABLICY
+            double maxNumber = FindBiggestValue.Max();
+            int position = Array.IndexOf(FindBiggestValue, maxNumber);
+            //Console.WriteLine("max number: " + maxNumber + "index" + position);
+            string chosenBookCategory = net[position + 7];
 
-            ViewBag.chosenBook = P_ChosenBook;
+            // WYŚWIETLAM NAZWĘ 
+            
+            //Console.WriteLine("najbardziej prawdopodobna kategoria książki: " + ChosenBookCategory);
+
+            //if (chosenBookCategory == "refWciag")
+                
+            //{
+            //    //Console.WriteLine("dobry wybór"); 
+            //}
+            //else
+            //{
+            //    //Console.WriteLine("inna kategoria niz refWciag");
+            //}
+
+
+
+            //// "Branie" indexu z konkretnej kategorii
+            //String[] aGatunekOutcomeIds = net.GetOutcomeIds("refWciag");
+            //for (outcomeIndex = 0; outcomeIndex < aGatunekOutcomeIds.Length; outcomeIndex++)
+            //{
+            //    if ("tak".Equals(aGatunekOutcomeIds[outcomeIndex]))
+            //    {
+            //        break;
+            //    }
+            //}
+
+            //double[] aValues = net.GetNodeValue("refWciag");
+            //double P_ChosenBook = aValues[outcomeIndex];
+
+            //ViewBag.ChosenBook = P_ChosenBook;
+            ViewBag.UserMood = jakiNastroj;
+            ViewBag.BookType = bookType;
+            ViewBag.MaxNumber = maxNumber;
+            ViewBag.Position = position;
+            ViewBag.ChosenBookCategory = chosenBookCategory;
 
             return View("Index");
         }
