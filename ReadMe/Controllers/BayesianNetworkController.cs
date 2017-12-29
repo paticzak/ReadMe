ab7665b2e89347fbfa69f2000436f969984456dc
paticzak <p.mundzik@gmail.com>
2017-12-29 09:41:34 +0100

    Working form with data from genie (usermood)

--- ReadMe/Controllers/BayesianNetworkController.cs
+++ ReadMe/Controllers/BayesianNetworkController.cs
@@ -8,9 +8,10 @@ using Smile.Learning;
 
 namespace ReadMe.Controllers
 {
+    [AllowAnonymous]
     public class BayesianNetworkController : Controller
     {
-        [AllowAnonymous]
+
         // GET: /BayesianNetwork/
         public ActionResult Index()
         {
@@ -24,8 +25,43 @@ namespace ReadMe.Controllers
             {
                 return View(e.Message);
             }
-           
+
             return View();
         }
-	}
+
+        [HttpPost]
+        public ActionResult RenderForm(string jakiNastroj)
+        {
+            
+            Network net = new Network();
+            ViewBag.Net = net;
+
+            net.ReadFile(@"C:\Users\Patrycja\Desktop\Praca inzynierska\nastroj-ksiazka-kategoria-okrojone-poprawione.xdsl");
+            //net.ReadFile(@"C:\Users\Patrycja\Desktop\Praca inzynierska\ReadMe2.xdsl");
+            net.UpdateBeliefs();
+            int outcomeIndex;
+            string nastroj1 = jakiNastroj;
+            //string nastroj1 = Request["jakiNastroj"];
+
+            // Getting the index of the "Failure" outcome:
+            net.SetEvidence("nastroj", nastroj1);
+            net.SetEvidence("wciagajaca", "tak");
+            net.UpdateBeliefs();
+            String[] aGatunekOutcomeIds = net.GetOutcomeIds("refWciag");
+            for (outcomeIndex = 0; outcomeIndex < aGatunekOutcomeIds.Length; outcomeIndex++)
+            {
+                if ("tak".Equals(aGatunekOutcomeIds[outcomeIndex]))
+                {
+                    break;
+                }
+            }
+
+            double[] aValues = net.GetNodeValue("refWciag");
+            double P_ChosenBook = aValues[outcomeIndex];
+
+            ViewBag.chosenBook = P_ChosenBook;
+
+            return View("Index");
+        }
+    }
 }
